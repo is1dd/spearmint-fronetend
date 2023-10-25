1313f55b3b98fb895c147fbae18c721928bd3f35
SIDDHARTH SHARMA <50225467+is1dd@users.noreply.github.com>
2023-10-25 14:26:15 +0530

    Update StockSelector.jsx

--- src/components/StockSelector.jsx
+++ src/components/StockSelector.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const StockSelector = ({ onSelect }) => {
   const stockList = ["abc", "stock1", "stock2", "stock3"];
+  const selectStyle = {
+    width: "25rem",
+  padding: "0.8rem 0.2rem"
+  }
 
   return (
     <div>
       <label>Select Stock: </label>
-      <select onChange={(e) => onSelect(e.target.value)}>
+      <select style={selectStyle} onChange={(e) => onSelect(e.target.value)}>
         <option value="">Select...</option>
         {stockList.map((stock) => (
           <option key={stock} value={stock}>
